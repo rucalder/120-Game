89498ccc10fb0ae20a30d2800b513722b282b386
Ryan Chapkis <rchapkis@ucsc.edu>
2020-05-18 18:40:44 -0700

    some comments added to make the custom pipeline class more clear

--- src/scenes/CustomPipeline2.js
+++ src/scenes/CustomPipeline2.js
@@ -1,3 +1,5 @@
+//this is a great website for learning about shaders (appart from the actual documentation through phaser):
+//  https://www.gamefromscratch.com/post/2014/05/16/OpenGL-Shader-Programming-Resources-Round-up.aspx
 var CustomPipeline2 = new Phaser.Class({
 
     Extends: Phaser.Renderer.WebGL.Pipelines.TextureTintPipeline,
@@ -10,19 +12,24 @@ var CustomPipeline2 = new Phaser.Class({
             game: game,
             renderer: game.renderer,
             fragShader: [
+            //this is the precison
             "precision mediump float;",
-
+            //these are the uniforms
             "uniform float     time;",
+            //this is the resolution 
             "uniform vec2      resolution;",
+            //this is the actual texture
             "uniform sampler2D uMainSampler;",
+            //this is the texture coordinate
             "varying vec2 outTexCoord;",
-
+            //this is the shader entry point
             "void main( void ) {",
 
                 "vec2 uv = outTexCoord;",
                 "//uv.y *= -1.0;",
                 "uv.y += (sin((uv.x + (time * 0.5)) * 10.0) * 0.1) + (sin((uv.x + (time * 0.2)) * 32.0) * 0.01);",
                 "vec4 texColor = texture2D(uMainSampler, uv);",
+                //this is the ultimate output of the shader
                 "gl_FragColor = texColor;",
 
             "}"
