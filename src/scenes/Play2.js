d19fed4297d861ed77e4232eaeefccfe77ffa43e
Ryan Chapkis <rchapkis@ucsc.edu>
2020-05-31 12:29:21 -0700

    picking up rum power up affects opponent

--- src/scenes/Play2.js
+++ src/scenes/Play2.js
@@ -509,7 +509,12 @@ class Play2 extends Phaser.Scene{
             loop: false
         });
         console.log('rum collision');
-        player.setGravityY(500)
+        if(player == this.player){
+            this.player2.setGravityY(500);
+        }
+        else{
+            this.player.setGravityY(500);
+        }
         this.stopPlayer = this.time.addEvent({delay: 5000, callback: function(){
             player.setGravityY(1000)
         }, callbackScope:this, loop: false});
