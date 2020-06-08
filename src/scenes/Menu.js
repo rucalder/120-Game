627127eb2096efb1448ad118bc054f6d62477c95
Ruben Calderon <rucalder@ucsc.edu>
2020-06-07 23:14:41 -0700

    Title screen implemented but non-functional

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,13 +4,13 @@ class Menu extends Phaser.Scene{
     }
 
     preload() {
-        // load audio
+      this.load.spritesheet('screen', "./assets/TitleScreen.png", { frameWidth: 1600, frameHeight: 1200 });
         
     }
 
     create(){
         //text display
-        let menuConfig = {
+        /*let menuConfig = {
             fontFamily: "Courier",
             fontSize: "26px",
             backgroundColor: "#F3B141",
@@ -26,14 +26,26 @@ class Menu extends Phaser.Scene{
         //show menu text
         let centerX = game.config.width/2;
         let centerY = game.config.height/2;
-        let textSpacer = 64;
+        let textSpacer = 64;*/
 
-        this.add.text(centerX, centerY - textSpacer, "Temp", menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY, "Press -> to start", menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = "#00FF00";
-        menuConfig.color = "#000";
+        //this.add.text(centerX, centerY - textSpacer, "Temp", menuConfig).setOrigin(0.5);
+        //this.add.text(centerX, centerY, "Press -> to start", menuConfig).setOrigin(0.5);
+        //menuConfig.backgroundColor = "#00FF00";
+        //menuConfig.color = "#000";
 
-        this.add.text(20, 20, "Temp Menu");
+        
+        this.titleScreen = this.add.sprite(this, 0, 0, "screen").setOrigin(0, 0)
+        this.anims.create({
+          key: "title",
+          repeat: -1,
+          frames: this.anims.generateFrameNumbers("screen", {start: 0, end: 2, first: 0}),
+          frameRate:8
+        });
+        //this.titleScreen = this.add.sprite(this, 0, 0).setOrigin(0, 0)
+        //this.bone = new Obstacle(this, 100, 100).setScale(1.5,1.5)
+        //this.titleScreen.play("title")
+
+        //this.add.text(20, 20, "Temp Menu");
         //this.scene.start("playScene");
 
         //define keys
@@ -45,9 +57,7 @@ class Menu extends Phaser.Scene{
         if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
           // easy mode
           game.settings = {
-            spaceshipSpeed: 3,
-            gameTimer1: 60000,
-            gameTimer2: 60000    
+              
           }
           //this.sound.play('sfx_select');
           this.scene.start("playScene");    
@@ -55,9 +65,7 @@ class Menu extends Phaser.Scene{
         if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
           // hard mode
           game.settings = {
-            spaceshipSpeed: 4,
-            gameTimer1: 45000,
-            gameTimer2: 45000    
+              
           }
           //this.sound.play('sfx_select');
           this.scene.start("playScene");    
